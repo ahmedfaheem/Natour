53ed9f251cdb61a8a777f1b82030d2e5cd4b6f19
Ahmed Faheem <ahmedfaheem499@gmail.com>
2026-07-19 02:37:28 +0300

    feat: configure environment variables for DB and  database connection

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const dotenv = require('dotenv');
+
 dotenv.config({ path: './config.env' });
+const mongoose = require('mongoose');
+
 const app = require('./app');
 
 //console.log(process.env.PORT);
 
 const PORT = process.env.PORT || 3000;
+const { DB_USER, DB_PASS } = process.env;
+
+mongoose
+  .connect(`mongodb+srv://${DB_USER}:${DB_PASS}@learn.pnb0djv.mongodb.net/`)
+  .then(() => {
+    console.log('Connected to DB');
+  })
+  .catch(() => {
+    console.log('Failed To Connect');
+  });
+
 app.listen(PORT, () => {
   console.log('Server Running......');
 });
