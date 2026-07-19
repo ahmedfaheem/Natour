949c698bc1225ad4057d713f60af1c0be064124d
Ahmed Faheem <ahmedfaheem499@gmail.com>
2026-07-19 22:35:10 +0300

    93- feat: expand tour model with additional fields for duration, maxGroupSize, difficulty, ratings, and descriptions

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,14 +5,57 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Name is Required'],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'Duration is Required'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'Max Group Size is Required'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'Difficulty is Required'],
+    enum: {
+      values: ['easy', 'medium', 'hard'],
+      message: 'Please choose a valid difficulty level',
+    },
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+    min: [1, 'Rating must be above 1.0'],
+    max: [5, 'Rating must be below 5.0'],
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'Price Required'],
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: [true, 'Description is Required'],
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'Image Cover is Required'],
+  },
+  images: [String],
+  startDates: [Date],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
   },
 });
 
