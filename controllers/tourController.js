60255f655ff6fb2e819b9a6fe8a34edc4b0eefbc
Ahmed Faheem <ahmedfaheem499@gmail.com>
2026-07-19 21:12:00 +0300

    92- feat: implement async handlers for tour operations in tourController

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,19 @@
 const Tour = require('../models/tourModel');
 
-exports.getAllTours = (req, res) => {};
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: 'success',
+      data: tours,
+    });
+  } catch (e) {
+    res.status(404).json({
+      status: 'fail',
+      error: e,
+    });
+  }
+};
 
 exports.createTour = async (req, res) => {
   try {
@@ -12,15 +25,58 @@ exports.createTour = async (req, res) => {
       data: newTour,
     });
   } catch (e) {
-    res.status(201).json({
+    res.status(400).json({
       status: 'fail',
       error: 'Invalid Data.',
     });
   }
 };
 
-exports.getTourByID = (req, res) => {};
+exports.getTourByID = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      data: tour,
+    });
+  } catch (e) {
+    res.status(404).json({
+      status: 'fail',
+      error: e,
+    });
+  }
+};
 
-exports.updateTour = (req, res) => {};
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, reqbody, {
+      runValidators: true,
+      returnDocument: 'after',
+    });
+    res.status(200).json({
+      status: 'success',
+      data: tour,
+    });
+  } catch (e) {
+    res.status(404).json({
+      status: 'fail',
+      error: e,
+    });
+  }
+};
 
-exports.deleteTour = (req, res) => {};
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (e) {
+    res.status(404).json({
+      status: 'fail',
+      error: e,
+    });
+  }
+};
