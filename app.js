d8b13c417a320289d1555ed2e280ea6930ffbf8b
Ahmed Faheem <ahmedfaheem499@gmail.com>
2026-07-24 20:42:24 +0300

    114: feat: implement global error handeler

--- app.js
+++ app.js
@@ -48,9 +48,21 @@ app.use('/api/v1/users', userRouter);
 
 // not found routes
 app.use((req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `The Route ${req.originalUrl}, you tried to fetch not found`,
+  const err = new Error(
+    `The Route ${req.originalUrl}, you tried to fetch not found`,
+  );
+  err.statusCode = 404;
+  err.status = 'fail';
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'fail';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 // 4- Server Configuration
