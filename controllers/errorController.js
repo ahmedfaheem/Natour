f985777419636fed6b087d41a2c16dae282dc754
Ahmed Faheem <ahmedfaheem499@gmail.com>
2026-07-25 02:08:38 +0300

    118:  feat: refactor error handling in errorController to separate production and development responses

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,10 +1,34 @@
+const sendErrorProd = (err, res) => {
+  if (err.isOperational === true) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: 'Somthing went wrong',
+    });
+  }
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
 module.exports = (err, req, res, next) => {
   //   console.log(err.stack);
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'fail';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
