fa0d0c7c74ac0749e798beb4daf41b95dc77690c
Ahmed Faheem <ahmedfaheem499@gmail.com>
2026-07-26 04:29:03 +0300

    127: feat: add password hashing and validation to user model

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { default: isEmail } = require('validator/lib/isEmail');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -23,11 +24,27 @@ const userSchema = new mongoose.Schema({
     minlength: [8, 'password must at least 8 charachter'],
   },
   passwordConfirm: {
+    // only for validation
     type: String,
     required: [true, 'please provide your password'],
+    validate: {
+      // work only with SAVE (create or update) not findByIdAndUodate
+      // can use User.save to update
+      validator: function (val) {
+        return this.password === val;
+      },
+      message: 'passwords are not the same',
+    },
   },
 });
 
+userSchema.pre('save', async function () {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 12);
+    // delete passwordConfirm becuase it used only for validation
+    this.passwordConfirm = undefined;
+  }
+});
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
