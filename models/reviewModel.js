f7ed883c342d78994d40b0c0416d6ff2cb288a15
Ahmed Faheem <ahmedfaheem499@gmail.com>
2026-08-04 17:36:32 +0300

    154: feat: add review model with schema for user reviews on tours

---
+++ models/reviewModel.js
@@ -0,0 +1,37 @@
+const { default: mongoose } = require('mongoose');
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'review can not be empty'],
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'review must belong to a tour'],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'review must belong to user'],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    toObject: { virtuals: true },
+    toJSON: { virtuals: true },
+  },
+);
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
